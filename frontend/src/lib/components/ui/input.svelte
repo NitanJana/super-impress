f47cbaddbe21a7d3c3e6769d5d682a89a3130f30
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2025-11-24 19:27:41 +0530

    bindable value prop in Input component

--- frontend/src/lib/components/ui/input.svelte
+++ frontend/src/lib/components/ui/input.svelte
@@ -2,7 +2,7 @@
 	import { cn } from '$lib/utils/cn';
 	import type { HTMLInputAttributes } from 'svelte/elements';
 
-	let { class: className, ...restProps }: HTMLInputAttributes = $props();
+	let { value = $bindable(), class: className, ...restProps }: HTMLInputAttributes = $props();
 </script>
 
-<input class={cn('input', className)} {...restProps} />
+<input bind:value class={cn('input', className)} {...restProps} />
